50cc33a6044a0135bb0e22cc82685099e873ee0e
Mara <mara-li@outlook.fr>
2022-06-08 11:34:46 +0200

    style: remove un-necessary semicolons

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,12 @@
-export function changePathVisibility(path: string, hide: boolean) {
+export async function changePathVisibility(path: string, hide: boolean) {
 	let n = document.querySelector(`[data-path="${path}"]`);
 	if (!n) {
 		return;
-	};
+	}
 	let p = n.parentElement
 	if (hide) {
 		p.style.display = `none`
 	} else {
 		p.style.display = ``;
-	};
-};
+	}
+}
